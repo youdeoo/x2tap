413f3406657df7588c7d7acc6f4b32f0fa8f4aa3
Holli_Freed <x2360415889@outlook.com>
2019-04-03 19:24:44 +0800

    移除多余的驱动文件和增加依赖文件检查

--- x2tap/x2tap.cs
+++ x2tap/x2tap.cs
@@ -1,20 +1,49 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 using x2tap.Utils;
 using x2tap.View;
 
 namespace x2tap
 {
-    public static class x2tap
-    {
+	public static class x2tap
+	{
         /// <summary>
         ///     应用程序的主入口点
         /// </summary>
         [STAThread]
         public static void Main()
         {
-            // 检查 TUN/TAP 适配器
-            if (TUNTAP.GetComponentId() == "" && !TUNTAP.Create())
+#if !DEBUG
+			var filenames = new string[]
+			{
+				"driver\\OemVista.inf",
+				"driver\\tap0901.cat",
+				"driver\\tap0901.sys",
+				"tapinstall.exe",
+				"tun2socks.exe",
+				"geoip.dat",
+				"geosite.dat",
+				"v2ctl.exe",
+				"v2ray.exe",
+				"wv2ray.exe"
+			};
+
+			// 依赖文件检查
+			foreach (var filename in filenames)
+			{
+				if (!File.Exists(filename))
+				{
+					MessageBox.Show(string.Format("缺失重要文件：{0}", filename), "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+					Application.Exit();
+					return;
+				}
+			}
+#endif
+
+			// 检查 TUN/TAP 适配器
+			if (TUNTAP.GetComponentId() == "" && !TUNTAP.Create())
             {
                 MessageBox.Show("尝试安装 TUN/TAP 适配器时失败！", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
 
